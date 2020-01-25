669f70b36b3f04cc3ecd02d3bf75b59a79abddb8
Xander C <xander.canedo@gmail.com>
2020-01-24 21:46:49 -0700

    dynamically rendering

--- src/App.js
+++ src/App.js
@@ -1,9 +1,63 @@
 import React from 'react';
 import './App.css';
-import Header from "./components/Header"
+import Header from "./components/Header";
+
+const characters = [
+  {
+    id: 1,
+    name: "Bob",
+    imageURL: "images/bob.webp"
+  },
+  {
+    id: 2,
+    name: "Linda",
+    imageURL: "images/linda.webp"
+  },
+  {
+    id: 3,
+    name: "Frond",
+    imageURL: "images/frond.webp"
+  },
+  {
+    id: 4,
+    name: "Gene",
+    imageURL: "images/gene.webp"
+  },
+  {
+    id: 5,
+    name: "jimmy jr",
+    imageURL: "images/jimmy_jr.webp"
+  },
+  {
+    id: 6,
+    name: "Jimmy Pesto",
+    imageURL: "images/jimmy_pesto.webp"
+  },
+  {
+    id: 7,
+    name: "Louise",
+    imageURL: "images/louise.webp"
+  },
+  {
+    id: 8,
+    name: "Mort",
+    imageURL: "images/mort.webp"
+  },
+  {
+    id: 9,
+    name: "Teddy",
+    imageURL: "images/teddy.webp"
+  },
+  {
+    id: 10,
+    name: "Tina",
+    imageURL: "images/tina.webp"
+  }
+]
+
 
 function App() {
-  return <Header/>
+  return <Header characters={characters}/>
 }
 
 export default App;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,29 +1,15 @@
 import React, { Component } from "react";
 import Footer from "../Footer";
 
-class Header extends Component {
-    // this will set the initial state
-    state = {
-        clicked: ""
-    }
-
-    render() {
-        return (
-            <div>
-                <img src="images/bob.webp"/>
-                <img src="images/linda.webp"/>
-                <img src="images/frond.webp"/>
-                <img src="images/gene.webp"/>
-                <img src="images/jimmy_jr.webp"/>
-                <img src="images/jimmy_pesto.webp"/>
-                <img src="images/louise.webp"/>
-                <img src="images/mort.webp"/>
-                <img src="images/teddy.webp"/>
-                <img src="images/tina.webp"/>
-                <Footer/>
-            </div>
-        );
-    }
+function Header(props) {
+    return (
+        <div>
+            {props.characters.map(item => (
+                <img src={item.imageURL} key={item.id}/>
+            ))}
+        <Footer/>
+    </div>
+    );
 }
 
 export default Header;
