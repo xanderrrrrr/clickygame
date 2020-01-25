9095aa888958637e8f0b5c2315fc90c59c9458cd
Xander C <xander.canedo@gmail.com>
2020-01-24 23:04:21 -0700

    dynamic css styling

--- src/components/Character/index.js
+++ src/components/Character/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import "./style.css";
 
 function Character(props) {
     return (
         <div className="card">
             <div className="img-container">
-                <img alt={props.name} src={props.image} onClick={() => props.handleClick(props.name)}/>
+                <img alt={props.name} src={props.image} onClick={() => props.handleClick(props.id)}/>
             </div>
         </div>
     );
